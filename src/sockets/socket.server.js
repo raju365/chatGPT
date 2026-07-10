5b8faa7897d23879c7a4fbd2c5b056aa61b2aa6f
raju barman <rajubarman7665@gmail.com>
2026-07-10 15:06:29 +0530

    socket.io implimented

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,11 +1,30 @@
 const { Server } = require("socket.io");
-
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
 function initSocketServer(httpServer) {
-
   const io = new Server(httpServer, {});
+  io.use(async(socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+    if (!cookies.token) {
+      next(new Error("Authentication error: no token provided"));
+    }
+    next();
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id);
+      socket.user = user;
+      next();
+
+
+    } catch (error) {
+      next(new Error("Authentication error: invalid token"));
+    }
+  });
 
   io.on("connection", (socket) => {
     console.log("New socket connection:", socket.id);
   });
 }
-module.exports = initSocketServer ;
+module.exports = initSocketServer;
