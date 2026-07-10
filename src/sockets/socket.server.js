9521985e1e0255d0ed8735ebdccb60e011ebb249
raju barman <rajubarman7665@gmail.com>
2026-07-10 23:13:29 +0530

    gemini ai intregated

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -2,29 +2,46 @@ const { Server } = require("socket.io");
 const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
+const aiService = require("../service/ai.service");
+
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
-  io.use(async(socket, next) => {
+  io.use(async (socket, next) => {
     const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
 
     if (!cookies.token) {
       next(new Error("Authentication error: no token provided"));
     }
-    next();
+
     try {
       const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
       const user = await userModel.findById(decoded.id);
       socket.user = user;
       next();
-
-
     } catch (error) {
       next(new Error("Authentication error: invalid token"));
     }
   });
 
   io.on("connection", (socket) => {
-    console.log("New socket connection:", socket.id);
+    socket.on("ai-message", async (messagePayload) => {
+      try {
+        console.log("Received ai-message:", messagePayload);
+        const response = await aiService.generateResponse(
+          messagePayload.content,
+        );
+
+        socket.emit("ai-response", {
+          content: response,
+          chat: messagePayload.chat,
+        });
+      } catch (error) {
+        console.error("Error generating AI response:", error.message);
+        socket.emit("ai-error", {
+          message: "Failed to generate AI response",
+        });
+      }
+    });
   });
 }
 module.exports = initSocketServer;
